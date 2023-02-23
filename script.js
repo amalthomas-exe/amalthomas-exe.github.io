aedb64fbb0f9e996ede0541f1b51a4a88b17b237
Amal Thomas <thomasamal479@gmail.com>
2023-02-23 22:42:26 +0530

    New inspiration

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const fname = document.getElementById("fname")
-const lname = document.getElementById("lname")
+const fname = document.getElementById("fname-main")
+const lname = document.getElementById("lname-main")
 
 const fnameLetters = fname.innerHTML.split("");
 const lnameLetters = lname.innerHTML.split("");
@@ -21,12 +21,9 @@ lnameLetters.forEach((letter)=>{
     lname.appendChild(lnameSpan)
 })
 
-document.onmousemove = function(event) {
-    console.log("Mouse X: " + event.clientX + ", Mouse Y: " + event.clientY);
-    var x = window.innerWidth - event.clientX;
-    var y = window.innerHeight - event.clientY;
-    let angle = 90 - Math.atan(x/y);
-    document.getElementById("circle").style.rotate = angle+"deg";
-    console.log(angle)
-  };
-  
+document.getElementById("reveal-btn").onclick = ()=>{
+    document.getElementById("cover-page").style.animation = "cover-go-up 1s ease-in-out";
+    setTimeout(()=>{
+        document.getElementById("cover-page").style.display = "none";
+    },1000)
+}
